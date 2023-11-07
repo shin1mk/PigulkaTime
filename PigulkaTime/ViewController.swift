eb5c6bf7c06eabadcbb3c78f38fc5b7b9b977ae5
MK1 <shin.m.dp@gmail.com>
2023-11-07 18:35:20 +0200

    Initial Commit

---
+++ PigulkaTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PigulkaTime
+//
+//  Created by SHIN MIKHAIL on 07.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
