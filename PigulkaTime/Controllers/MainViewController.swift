906a1bc041750c92c8021791072798a0f30183f1
MK1 <shin.m.dp@gmail.com>
2023-11-07 19:05:05 +0200

    start

--- PigulkaTime/Controllers/MainViewController.swift
+++ PigulkaTime/Controllers/MainViewController.swift
@@ -6,12 +6,12 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+final class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
---
+++ PigulkaTime/textHelper.swift
@@ -0,0 +1,8 @@
+//
+//  textHelper.swift
+//  PigulkaTime
+//
+//  Created by SHIN MIKHAIL on 07.11.2023.
+//
+
+import Foundation
